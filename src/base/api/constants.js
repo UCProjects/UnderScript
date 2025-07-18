69061728c2d73356692c075c0dbb39f395a12fe2
feildmaster <feildmaster@users.noreply.github.com>
2025-07-18 17:55:10 -0500

    chore: deprecate constants.items

--- src/base/api/constants.js
+++ src/base/api/constants.js
@@ -1,7 +1,18 @@
 import * as api from 'src/utils/4.api.js';
 import Item from 'src/structures/constants/item.js';
+import Priority from 'src/structures/constants/priority';
 
 const constants = api.mod.constants;
-Object.keys(Item).forEach((key) => {
-  constants[key] = Item[key];
-});
+Object.entries(Item).forEach((([key, value]) => {
+  Object.defineProperty(constants, key, {
+    get() {
+      console.warn(`'underscript.constants.${key}' is deprecated, use 'underscript.items.${key}' instead.`);
+      return value;
+    },
+  });
+}));
+
+constants.getItem = (value) => Item.find(value);
+constants.isItem = (other) => Item.find(other) !== undefined;
+constants.getPriority = (value) => Priority.find(value);
+constants.isPriority = (other) => Priority.find(other) !== undefined;
