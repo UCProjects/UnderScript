00f0d517d2a30aef456e7f25a37544a184940988
feildmaster <feildmaster@users.noreply.github.com>
2026-07-31 19:24:51 -0500

    fix: new chat window renders properly

--- src/base/styles/chat.js
+++ src/base/styles/chat.js
@@ -3,6 +3,4 @@ import style from 'src/utils/style.js';
 style.add(
   '.chat-message { overflow-wrap: break-word; }', // Break text
   '.chat-messages { user-select: text; }', // Always allow chat to be selected
-  '.chat-messages { height: calc(100% - 30px); }', // Fix chat window being all funky with sizes
-  '.chat-messages { min-height: 100px; }', // Fix chat getting too small
 );
